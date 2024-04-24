8e46ae82f3cca7de3a4d1394f518ac22d7bb5aff
Simon Kostenko <semenkostencko@gmail.com>
2024-04-24 20:58:26 +0300

    Added a new project without storyboards

---
+++ ArtObjects/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  ArtObjects
+//
+//  Created by Simon Kostenko on 24.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+}
